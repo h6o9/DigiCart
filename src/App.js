fc95ad152cd918d39e401508ce7b528da8c99a4a
Shahzaib <samiafzaal687@gmail.com>
2024-12-27 04:04:20 +0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-  import React from "react";
-  import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-  import Navbar from "./components/Navbar"; // Adjust path
-  import Home from "./pages/Home"; // Adjust path
-  import Shop from "./pages/Shop"; // Adjust path
-  import CategoryPage from "./pages/CategoryPage"; // Adjust path
-  import CartPage from "./pages/CartPage"; // Adjust path
-  import ContactUs from "./pages/ContactUs"; // Adjust path
-  import Footer from "./components/Footer"; // Adjust path
-  import ProductDetails from "./pages/ProductDetails"; // Adjust path
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
+import ContactUs from "./pages/ContactUs";
+import Footer from "./components/Footer";
+import ProductDetails from "./pages/ProductDetails";
 
-  const App = () => {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/category/:category" element={<CategoryPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-        </Routes>
-        <Footer />
-      </Router>
-    );
-  };
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/category/:category" element={<CategoryPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
-  export default App;
+export default App;
